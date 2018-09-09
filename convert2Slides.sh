2f677a169bb3ceb42b378d352bd6e3136d4646d2
Marco Saletta <marcosaletta@MacBook-Pro-di-Marco.local>
2018-09-09 15:43:05 +0200

    Update index.html

--- convert2Slides.sh
+++ convert2Slides.sh
@@ -2,7 +2,7 @@
 
 jupyter nbconvert --to slides $1 --reveal-prefix=reveal.js --SlidesExporter.reveal_theme=night --SlidesExporter.reveal_scroll=True --SlidesExporter.reveal_transition=none
 
-name="$(cut -d'_' -f1 <<<"$1")"
+name="$(cut -d'.' -f1 <<<"$1")"
 mv $name.slides.html $name.html 
 git commit -am"Update index.html"
 git push -u origin master
